32434500c00858d752daee414e1a42e35623f34b
Trishit Hazra <hazratrishit@gmail.com>
2024-08-17 11:37:48 +0530

    Moved the replaceTemplate function into a separate file and imported it here

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //<------------------------------------- FILE READING AND WRITING --------------------------------->//
 
 // Blocking, synchronous code
@@ -50,23 +52,6 @@ const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overvi
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-// we use the replace method to replace the placeholder with the data from data.json
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    }
-
-    return output;
-}
 
 // everytime we make a request, this createServer function is executed
 const server = http.createServer((req, res) => {
