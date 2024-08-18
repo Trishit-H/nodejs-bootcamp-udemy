e2753b2fa35d652275ea709907d785469a9d977a
Trishit Hazra <hazratrishit@gmail.com>
2024-08-18 17:56:52 +0530

    finished the project

--- 1-node-book-list(project)/index.js
+++ 1-node-book-list(project)/index.js
@@ -8,19 +8,54 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 // converting the JSON string into JavsScript object
 const dataObj = JSON.parse(data);
 
+// reading all the html templates
+const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const cardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const bookTemplate = fs.readFileSync(`${__dirname}/templates/template-book.html`, 'utf-8');
+
+const replacePlaceholder = (template, book) => {
+    let output = template.replace(/{%BOOK_NAME%}/g, book.bookName);
+    output = output.replace(/{%AUTHOR_NAME%}/g, book.authorName);
+    output = output.replace(/{%GENRE%}/g, book.genre);
+    output = output.replace(/{%PUBLICATION_DATE%}/g, book.publicationDate);
+    output = output.replace(/{%DESCRIPTION%}/g, book.description);
+    output = output.replace(/{%ID%}/g, book.id);
+
+    return output;
+}
+
 const server = http.createServer((req, res) => {
 
     // destructuring the query and pathname from the url using url.parse method
-    const { query, pathname } = url.parse(req.url);
+    const { query, pathname } = url.parse(req.url, true);
 
     // route for home/overview page
     if (pathname === '/' || pathname === '/overview') {
-        res.end('This is OVERVIEW!')
+        // content type to html since we are sending an html page
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardHTML = dataObj.map(el => replacePlaceholder(cardTemplate, el)).join();
+
+        const finalHTML = overviewTemplate.replace(/{%BOOK_CARDS%}/g, cardHTML);
+
+        res.end(finalHTML)
     }
 
     // route to display details of book
     else if (pathname === '/product') {
-        res.end('This is PRODUCT!')
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const book = dataObj.filter(el => el.id === Number(query.id))[0];
+        console.log(book);
+
+
+        const output = replacePlaceholder(bookTemplate, book);
+
+        res.end(output);
     }
 
     // test route to display the content of data.json
