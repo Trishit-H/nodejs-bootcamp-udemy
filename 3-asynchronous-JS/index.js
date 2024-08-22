fcd13c0954629bbb4811d88052e9eed22d8ead4f
Trishit Hazra <hazratrishit@gmail.com>
2024-08-22 11:47:10 +0530

    Implement dog breed image fetch with callback-based approach
    - Read dog breed from `dog.txt` using `fs.readFile`
    - Fetch a random image of the breed from the Dog API with `superagent`
    - Save the image URL to `dog-img.txt` using `fs.writeFile`
    - Included error handling for file operations and API requests
    
    Note: The use of nested callbacks leads to callback hell, reducing code readability and maintainability. Consider refactoring with promises or async/await for improved code structure.
    Read code comments

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,31 @@
+// What are we gonna do - we are gonna read the breed of the dog from the dog.txt file and do an http request to the get the image of that dog breed and save it to another text file.
+// Alll will be done by callback functions to see the problem
+// API - https://dog.ceo/api/breed/hound/images/random
+// replace 'hound' with any dog breed
+
+const fs = require('fs');
+const superagent = require('superagent'); // to send api requests
+
+fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
+    if (err) return console.log(err.message);
+    console.log(`Breed: ${data}`);
+
+    superagent
+        // get method to send request to the url
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+        // first parameter - error, second parameter - resullt/response
+        // the response of the api is in res.body
+        .end((err, res) => {
+            if (err) return console.log(err.message);
+            console.log(res.body.message); // message contains the image url
+
+            // then write the data into a file
+            fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, err => {
+                if (err) return console.log('Error writing file:', err.message);
+                console.log('File written!');
+            });
+        });
+});
+
+// here 3 callbacks are nested each other.
+// with even more callbacks, this coudl lead to bad code readability and makes it hard to maintain
