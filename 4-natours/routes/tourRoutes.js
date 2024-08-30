74f9a672ec123b5fd714ab26aa2448602c2e0cbb
Trishit Hazra <hazratrishit@gmail.com>
2024-08-30 20:56:51 +0530

    here we chained the checkReqBody middleware in the post route

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -7,13 +7,15 @@ const {
   updateTour,
   deleteTour,
   checkId,
+  checkReqBody,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
+// this middleware is for checking if the param containing the id is valid or not
 router.param('id', checkId);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkReqBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
