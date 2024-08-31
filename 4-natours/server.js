0f82ac83386cea315310a902f07da5c1ccb36f19
Trishit Hazra <hazratrishit@gmail.com>
2024-08-31 08:41:14 +0530

    required dotenv package and loaded the config.env files containing our variavles to process.env

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -3,10 +3,14 @@ We created this file because it's good practice to have everything related to th
 
 server.js is our main file, where everything starts
 */
+const dotenv = require('dotenv');
+
+// what this does is it loads the content of config.env file to process.env
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}`);
