be7517ae3df7d1dd6445b25db552e0e4aa4b432c
Trishit Hazra <hazratrishit@gmail.com>
2024-09-01 12:59:44 +0530

    removed few middlewares

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -7,7 +7,6 @@ const {
   updateTour,
   deleteTour,
   // checkId,
-  checkReqBody,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -15,7 +14,7 @@ const router = express.Router();
 // this middleware is for checking if the param containing the id is valid or not
 // router.param('id', checkId);
 
-router.route('/').get(getAllTours).post(checkReqBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
