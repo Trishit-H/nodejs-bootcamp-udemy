988b4ffe2295657af603ee973bb1de76cd4b816c
Trishit Hazra <hazratrishit@gmail.com>
2024-09-01 09:50:21 +0530

    connected to database

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -4,11 +4,25 @@ We created this file because it's good practice to have everything related to th
 server.js is our main file, where everything starts
 */
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 // what this does is it loads the content of config.env file to process.env
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+// here we replace <PASSWORD> in the connection string with the actual password
+const DB_URI = process.env.MONGODB_URI.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+// connecting to database
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log('Connection to database successful');
+  })
+  .catch((err) => {
+    console.log('Error connecting to database:', err.message);
+  });
 
 const PORT = process.env.PORT || 3000;
 
