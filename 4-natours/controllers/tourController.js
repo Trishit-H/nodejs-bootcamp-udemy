c3aae5ae39c1b69c15c96c8903e601f2268d35ea
Trishit Hazra <trishitgithub@gmail.com>
2024-09-05 12:51:27 +0530

    Refactored the api features

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -9,69 +9,117 @@ const aliasTopTours = (req, res, next) => {
   next();
 };
 
+/**
+ * APIFeatures class is responsible for applying query operations such as filtering, sorting,
+ * field limiting, and pagination to a Mongoose query object based on the incoming HTTP request parameters.
+ *
+ * It enables chaining of methods to refine the query before executing it against the database.
+ */
+class APIFeatures {
+  constructor(query, queryString) {
+    // The `query` is the Mongoose query object that will be used to interact with the database.
+    // The `queryString` is the HTTP request query parameters (e.g., req.query) received from the client.
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    // Create a shallow copy of the query parameters (queryString) from the request.
+    // This ensures we don't modify the original object while manipulating it.
+    const queryObject = { ...this.queryString };
+
+    // Define an array of fields that should be excluded from filtering.
+    // These fields (e.g., `page`, `sort`, `limit`, `fields`) are used for other operations like pagination, sorting, etc.
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    // Loop through the `excludedFields` array and delete each of these fields from the `queryObject`.
+    // This prevents them from being used as part of the filtering conditions.
+    excludedFields.forEach((el) => delete queryObject[el]);
+
+    // ADVANCED FILTERING:
+    // Convert the `queryObject` to a string so that we can manipulate it using regular expressions.
+    // We aim to transform query parameters like `gte`, `gt`, `lte`, `lt` into MongoDB operators (`$gte`, `$gt`, `$lte`, `$lt`).
+    let queryStr = JSON.stringify(queryObject);
+
+    // Use a regular expression to find and replace the query operators (gte, gt, lte, lt) with their corresponding MongoDB operators.
+    // For example, `duration[gte]=5` becomes `{ duration: { $gte: 5 } }`.
+    queryStr = queryStr.replace(/\b(?:gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // Parse the modified query string back into an object and use it to build the Mongoose query.
+    // The `find()` method does not execute the query but returns a Mongoose Query object, allowing for further chaining.
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    // Return `this` to allow for method chaining in the `APIFeatures` class.
+    return this;
+  }
+
+  // Method for sorting the query results based on the fields specified in the request query string.
+  sort() {
+    if (this.queryString.sort) {
+      // If the `sort` parameter is provided in the query string, convert it into a format
+      // that Mongoose can use. The sort fields are separated by commas in the request,
+      // but MongoDB expects space-separated fields.
+      const sortBy = this.queryString.sort.split(',').join(' ');
+
+      // Chain the `sort()` method to the query and pass the formatted sort fields.
+      this.query = this.query.sort(sortBy);
+    } else {
+      // If no sort parameter is provided, the default sorting is by `ratingsAverage` in descending order.
+      this.query = this.query.sort('-ratingsAverage');
+    }
+
+    // Return `this` to enable method chaining in the `APIFeatures` class.
+    return this;
+  }
+
+  // Method for limiting the fields returned in the query results.
+  limitingFields() {
+    if (this.queryString.fields) {
+      // If the `fields` parameter is provided in the query string, convert it into a format
+      // that Mongoose can use. The requested fields are separated by commas in the request,
+      // but MongoDB expects space-separated fields.
+      const selectFields = this.queryString.fields.split(',').join(' ');
+
+      // Chain the `select()` method to the query and pass the formatted fields to be selected.
+      this.query = this.query.select(selectFields);
+    } else {
+      // If no `fields` parameter is provided, exclude certain fields (e.g., `__v`, `createdAt`, `updatedAt`)
+      // from the results by default. The `-` sign indicates exclusion.
+      this.query = this.query.select('-__v -createdAt -updatedAt');
+    }
+
+    // Return `this` to enable method chaining in the `APIFeatures` class.
+    return this;
+  }
+
+  // Method for implementing pagination in the query results.
+  paginate() {
+    // Convert the `page` and `limit` query parameters into numbers, and set default values
+    // if they are not provided. `page` defaults to 1, and `limit` defaults to 100.
+    const page = Number(this.queryString.page) || 1;
+    const limit = Number(this.queryString.limit) || 100;
+
+    // Calculate the number of documents to skip based on the current page.
+    // For example, on page 2 with a limit of 10, you would skip the first 10 results.
+    const skip = (page - 1) * limit;
+
+    // Chain the `skip()` and `limit()` methods to the query to implement pagination.
+    this.query = this.query.skip(skip).limit(limit);
+
+    // Return `this` to enable method chaining in the `APIFeatures` class.
+    return this;
+  }
+}
+
 // function to get all tours
 const getAllTours = async (req, res) => {
   try {
-    // B U I L D    Q U E R Y
-    // 1a) FILTERING
-
-    // create a copy of the req.query object
-    const queryObject = { ...req.query };
-
-    // these are the query params that we don't want to search by
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-    // remove the fields present in excludedFields array from queryObject
-    excludedFields.forEach((el) => delete queryObject[el]);
-
-    // 1b) ADVANCED FILTERING
-    // here we replace the operator coming from req.query with mongodb operator
-    // operators to replace - gte, gt, lte, lt with $gte, $gt, $lte, $lt
-
-    // first convert the query object to a string
-    let queryString = JSON.stringify(queryObject);
-    queryString = queryString.replace(/\b(?:gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    // we build our Query object here, with the filtering query and store it in the query variable
-    let query = Tour.find(JSON.parse(queryString));
-
-    // 2) SORTING
-    // check if we have a sort query in the query param
-    if (req.query.sort) {
-      // to get a string in the following format "field1 field2..." from "field1,field2..."
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy); // if there is, then chain the sort method and pass in sortBy as the argument
-    } else {
-      query = query.sort('-ratingsAverage'); // if no sort query is given, sort by ratingsAverage field
-    }
-
-    // 3) FIELD LIMITING
-    if (req.query.fields) {
-      const selectFields = req.query.fields.split(',').join(' ');
-      query = query.select(selectFields);
-    } else {
-      query = query.select('-__v -createdAt -updatedAt');
-    }
-
-    // 4) PAGINATION
-    // page=2&limit=10 ---> page 1, 1-10 docs; page 2, 11-20 docs; page 3, 21-30 docs ...
-
-    const page = Number(req.query.page) || 1; // convert the page value to a number and have a default value of 1
-    const limit = Number(req.query.limit) || 100; // convert the limit value to a number and have a default value of 1
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    // this is to check if we have any more document to show
-    if (req.query.page) {
-      const docCount = await Tour.countDocuments();
-      if (skip >= docCount) {
-        throw new Error("This page doesn't exist");
-      }
-    }
-
     // E X E C U T E   Q U E R Y
-    const tours = await query.exec();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .limitingFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
