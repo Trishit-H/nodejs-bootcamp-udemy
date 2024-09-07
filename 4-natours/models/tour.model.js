3b6600431528bb488e77e0f0ae8cb358c83f4fd5
Trishit Hazra <trishitgithub@gmail.com>
2024-09-07 21:25:26 +0530

    Implememted document middleware using pre and post method on the "save" hook

--- 4-natours/models/tour.model.js
+++ 4-natours/models/tour.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // Define the tour schema which will outline the structure of each document in the "tours" collection
 // The schema enforces specific data types and validation rules for each field
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -58,13 +60,38 @@ const tourSchema = new mongoose.Schema(
     timestamps: true,
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
+  },
 );
 
 // durationInWeeks - a virtual property that calculates and returns the duration in weeks based on the `duration` field (assumed to be in days).
 // This property won't be stored in the database but can be accessed like a normal field when retrieving documents.
 tourSchema.virtual('durationInWeeks').get(function () {
   return (this.duration / 7).toFixed(1); // Convert days to weeks and round to one decimal place.
+});
+
+// DOCUMENT MIDDLEWARE: Executes before .save() and .create(), but not for .insertMany().
+// The 'save' is a pre-save hook, which allows us to run custom logic before saving the document to the database.
+// The 'this' keyword refers to the document instance that is being saved.
+// In this middleware, we generate a slug from the document's name field and assign it to the slug property.
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// DOCUMENT MIDDLEWARE: Pre-save hook that runs before a document is saved to the database.
+// This middleware will execute right before the document is persisted to the database.
+// We can use this hook for logging, validation, or modifying the document before saving.
+tourSchema.pre('save', function (next) {
+  console.log('Will save document to database...');
+  next();
+});
+
+// DOCUMENT MIDDLEWARE: Post-save hook that runs after a document has been saved to the database.
+// This middleware executes after the document has been successfully persisted.
+// The 'doc' argument refers to the saved document, which can be logged, processed, or used for further actions.
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
 });
 
 // Create the Tour model using the defined schema
