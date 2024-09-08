c96846651b54d02c5f026cf500e1fdff8f8833e3
Trishit Hazra <trishitgithub@gmail.com>
2024-09-08 09:32:58 +0530

    Data Validation: added some built-in mongoose validators

--- 4-natours/models/tour.model.js
+++ 4-natours/models/tour.model.js
@@ -10,6 +10,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxLength: [40, 'A tour name must have equal or less than 40 characters'],
+      minLength: [10, 'A tour name must have equal or more than 10 characters'],
     },
     slug: String,
     duration: {
@@ -23,10 +25,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be equal or above 1.0'],
+      max: [5, 'Rating must be equal or less 5.0'],
     },
     ratingsQuantity: {
       type: Number,
