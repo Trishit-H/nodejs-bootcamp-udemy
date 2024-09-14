4c6ea8ff7a73b8d342df40ce6b0dd559a8354a82
Trishit Hazra <trishitgithub@gmail.com>
2024-09-14 22:00:21 +0530

    created a custom error class that extends from the built-in js Error class

---
+++ 4-natours/utils/appError.js
@@ -0,0 +1,26 @@
+// Define a custom error class that extends the built-in Error class
+class AppError extends Error {
+  // Constructor method to initialize the custom error
+  constructor(message, statusCode) {
+    // Call the parent class (Error) constructor with the message
+    super(message);
+
+    // Assign the HTTP status code to the instance property
+    this.statusCode = statusCode;
+
+    // Determine if the error is a client error (4xx) or server error (5xx)
+    // and set the status property accordingly
+    this.status = statusCode.toString().startsWith('4') ? 'fail' : 'error';
+
+    // Custom property to indicate if the error is operational
+    // An operational error is expected and can be handled by the application
+    this.isOperational = true;
+
+    // Capture the stack trace for the error instance
+    // Excludes the current constructor from the stack trace to make it cleaner
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+// Export the AppError class for use in other modules
+module.exports = AppError;
