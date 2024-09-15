949829791602826964d30e397d28ce177d13ae33
Trishit Hazra <trishitgithub@gmail.com>
2024-09-15 21:17:06 +0530

    In production error we implemented a check whether the error occured is operational or not. if it is operational, we send the details of the error otherwise we send a generic error for programming error

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -12,12 +12,25 @@ const sendErrorDevelopment = (err, res) => {
 // Function that generates error that will be sent during production
 // In production mode, we provide only essential error information to avoid exposing sensitive details
 const sendErrorProduction = (err, res) => {
-  // Send the error response with minimal information
-  res.status(err.statusCode).json({
-    status: err.status, // Provide the status (e.g., 'fail' or 'error')
-    message: err.message, // Provide a generic error message (should be user-friendly)
-    // In production, we avoid sending the full error details or stack trace for security reasons
-  });
+  if (err.isOperational) {
+    // Here we would only send the error if it is an operational one
+    // Send the error response with minimal information
+    res.status(err.statusCode).json({
+      status: err.status, // Provide the status (e.g., 'fail' or 'error')
+      message: err.message, // Provide a generic error message (should be user-friendly)
+      // In production, we avoid sending the full error details or stack trace for security reasons
+    });
+  } else {
+    // For programming errors, we send a generic error message to the client
+    // And we print the error in the console for us developers to see
+
+    console.error('ERROR!!!!', err);
+
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went very wrong!',
+    });
+  }
 };
 
 /**
