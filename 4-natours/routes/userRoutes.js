7577dcfe66485abd15a8ad9944c9b1c1a40ee53f
Trishit Hazra <trishitgithub@gmail.com>
2024-10-09 18:35:32 +0530

    added a route(`/login`) for logging in users

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -8,12 +8,13 @@ const {
   deleteUser,
 } = require('../controllers/userController');
 
-const { signUp } = require('./../controllers/authController');
+const { signUp, login } = require('./../controllers/authController');
 
 const router = express.Router();
 
-// This route is for signning up users
+// This routes are for signning up, logging in users
 router.post('/signup', signUp);
+router.post('/login', login);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
