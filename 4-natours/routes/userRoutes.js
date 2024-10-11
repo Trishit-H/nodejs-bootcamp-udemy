9d1a2dae4d37ceb066cfcf0c2ffcd666250a02d2
Trishit Hazra <trishitgithub@gmail.com>
2024-10-11 08:03:45 +0530

    Changed the http verb of /reset-password route from POST to PATCH

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -8,13 +8,22 @@ const {
   deleteUser,
 } = require('../controllers/userController');
 
-const { signUp, login } = require('./../controllers/authController');
+const {
+  signUp,
+  login,
+  forgotPassword,
+  resetPassword,
+} = require('./../controllers/authController');
 
 const router = express.Router();
 
 // This routes are for signning up, logging in users
 router.post('/signup', signUp);
 router.post('/login', login);
+
+// These routes are for forgot and reset password
+router.post('/forgot-password', forgotPassword);
+router.patch('/reset-password', resetPassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
