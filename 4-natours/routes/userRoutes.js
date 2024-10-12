f902f93db473445671621884ffa2f7449b39826a
Trishit Hazra <trishitgithub@gmail.com>
2024-10-12 11:29:29 +0530

    Added a route to let users to update only when they are LOGGED IN

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -13,6 +13,8 @@ const {
   login,
   forgotPassword,
   resetPassword,
+  updatePassword,
+  protectedRoute,
 } = require('./../controllers/authController');
 
 const router = express.Router();
@@ -20,6 +22,9 @@ const router = express.Router();
 // This routes are for signning up, logging in users
 router.post('/signup', signUp);
 router.post('/login', login);
+
+//Route for user to update their password when they are logged in
+router.patch('/updateMyPassword', protectedRoute, updatePassword);
 
 // These routes are for forgot and reset password
 router.post('/forgot-password', forgotPassword);
